d2facd501224209a2dac29f4ac6fd3b99badf8ef
Raphael Moutard <raphael.moutard@gmail.com>
2012-02-22 11:51:08 +0100

    Jslint tool

--- photoStream/transition.js
+++ photoStream/transition.js
@@ -2,19 +2,22 @@
 
 function add(psSrc) {
 
+  // jsLint : declare all the variables at the beginning of the functions
+  var lsImgTag, liImgWidth;
+
   // append the new image at the end of items div
-  var lsImgTag = '<img src="' + psSrc + '" alt="query image" />';
+  lsImgTag = '<img src="' + psSrc + '" alt="query image" />';
   $('.items').append(lsImgTag);
-  
+
   // Set the value of the first image margin-left
-  var liImgWidth = $('.items img:first').width();
+  liImgWidth = $('.items img:first').width();
   $('.items img:first').css('margin-left', '-' + liImgWidth + 'px');
 
   // Wait for the end of animation to remove the first image
-  $('.items img:last').bind(
-      'webkitTransitionEnd', 
-      function (e) {
-        $('.items img:first').remove();
-      }); 
-  
+  $('.items img:last').bind('webkitTransitionEnd',
+    function (e) {
+      $('.items img:first').remove();
+    }
+  );
+
 }
